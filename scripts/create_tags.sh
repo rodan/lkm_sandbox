ecffa41274d67c0ab24efec8b3fffd1c99e89eb1
Petre Rodan <2b4eda@subdimension.ro>
2023-10-19 19:39:43 +0300

    cleanup of fake_includes

--- scripts/create_tags.sh
+++ scripts/create_tags.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+mkdir -p /tmp/fake_includes/gnu
+touch /tmp/fake_includes/{stddef.h,stubs-soft.h,gnu/stubs-soft.h}
+
+
 touch depend
 
-makedepend -f depend  -I/usr/src/linux/include -I/usr/src/linux/arch/arm/include -I/usr/src/linux/arch/arm/include/generated/ -Ifake_includes $@
+makedepend -f depend  -I/usr/src/linux/include -I/usr/src/linux/arch/arm/include -I/usr/src/linux/arch/arm/include/generated/ -I/tmp/fake_includes $@
 
 echo './' > depmod_list
 grep ': ' depend | cut -d':' -f2 | sed 's/[ \t]*$//;s|^[ \t]*||g;s| |\n|g' >> depmod_list
